feb50f264297bd5d57d0487ed59a88116f3e3d3d
6rak0 <ego.6rak0@gmail.com>
2022-12-27 23:02:43 -0600

    feat: :sparkles: added Baileys provider

--- src/infrastructure/container.ts
+++ src/infrastructure/container.ts
@@ -2,6 +2,7 @@ import { ContainerBuilder } from "node-dependency-injection";
 import { MessageCreate } from '../application/message.create'
 import MessageCtrl from './controller/message.ctrl';
 import WsTransporter from './repositories/ws.external';
+import Baileys from './repositories/baileys';
 
 const container = new ContainerBuilder();
 
@@ -11,9 +12,12 @@ const container = new ContainerBuilder();
 container.register("ws.transporter", WsTransporter);
 const wsTransporter = container.get("ws.transporter");
 
+container.register("baileys", Baileys);
+const baileys = container.get("baileys");
+
 container
   .register("message.creator", MessageCreate)
-  .addArgument(wsTransporter);
+  .addArgument(baileys);
 
 const messageCreator = container.get("message.creator");
 
---
+++ src/infrastructure/repositories/baileys.ts
@@ -0,0 +1,79 @@
+import makeWASocket, {
+  useMultiFileAuthState,
+  DisconnectReason,
+} from "@adiwajshing/baileys";
+import MessageExternal from "../../domain/message.external.repository";
+
+class Baileys implements MessageExternal {
+    sock: any;
+    constructor () {
+        this.connect()
+    }
+    async connect(){
+        const { state, saveCreds } = await useMultiFileAuthState(".baileys_auth");
+        this.sock = makeWASocket({
+          printQRInTerminal: true,
+          auth: state,
+        });
+        this.sock.ev.on("creds.update", saveCreds);
+        this.sock.ev.on("connection.update", (update: any) => {
+          const { connection, lastDisconnect } = update;
+          if (connection === "close") {
+            const shouldReconnect =
+              lastDisconnect.error?.output?.statusCode !==
+              DisconnectReason.loggedOut;
+            console.log(
+              "connection closed due to ",
+              lastDisconnect.error,
+              ", reconnecting ",
+              shouldReconnect
+            );
+            // reconnect if not logged out
+            if (shouldReconnect) {
+              this.connect();
+            }
+            console.log(
+              "connection closed due to ",
+              lastDisconnect.error,
+              ", reconnecting "
+            );
+          } else if (connection === "open") {
+            console.log("opened connection");
+          }
+        });
+        this.sock.ev.on("messages.upsert", ({ messages }:any) => {
+          if (messages[0].message.conversation === `online`) {
+            console.log("replying to", messages[0].key.remoteJid);
+            this.sock.sendMessage(messages[0].key.remoteJid, { text: `online` });
+          } else {
+            console.log(JSON.stringify(messages, undefined, 2));
+          }
+        });
+    }
+  /**
+   * Enviar mensaje de WS
+   * @param message
+   * @returns
+   */
+  async sendMsg(message: { phone: string; text: string }): Promise<any> {
+    try {
+      const { phone, text } = message;
+      const response = await this.sock.sendMessage(`${phone}@s.whatsapp.net`, {text: text});
+      return response;
+    } catch (e: any) {
+      return Promise.resolve({ error: e.message });
+    }
+  }
+
+//   private generateImage = (base64: string) => {
+//     const path = `${process.cwd()}/temp`;
+//     let qr_svg = image(base64, { type: "svg", margin: 4 });
+//     qr_svg.pipe(require("fs").createWriteStream(`${path}/qr.svg`));
+//     console.log(` => scan code in /qr`);
+//   };
+//   private qrterminal = (base64: string) => {
+//     qrcode.generate(base64, { small: true });
+//   };
+}
+
+export default Baileys;
